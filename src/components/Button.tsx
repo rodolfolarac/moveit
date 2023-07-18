63720e084ad5a179fec0fb7e921b89e1c824ba7e
Rodolfo <rodolfo.cassaro@auaha.net>
2023-07-18 20:26:04 -0300

    exercicio de useState finalizado

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,15 +9,19 @@ export function Button(props: ButtonProps) {
     const [counter, setCounter] = useState(1)
 
     function increment() {
-        
+        setCounter(counter + 1);
     }
     return(
         <>
-        <button type="button" style={{backgroundColor: props.color}} >
+        <button 
+        type="button" 
+        style={{backgroundColor: props.color}}
+        onClick={increment} >
             {props.children} 
         </button>
+
         <p>
-        contador:<strong>{counter}</strong>
+        contador:{counter}
         </p>
         </>
     )

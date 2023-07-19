57f8616650e86cc87e119bc1c71b6374dab7407f
Rodolfo <rodolfo.cassaro@auaha.net>
2023-07-19 08:17:03 -0300

    configurações globais finalizadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
-import React from 'react';
-import { Button } from './components/Button';
+import "./styles/global.css"
 
 function App() {
   return (
     <>
-    <h1>Hello NLW</h1>
-    <Button color="green">
-      Botão 1
-    </Button>
-    <Button color="red">
-      Botão 2
-    </Button>
-    <Button color="pink">
-      Botão 3
-    </Button>
+    
     </>
   );
 }

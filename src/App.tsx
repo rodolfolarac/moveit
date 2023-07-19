3734c7fbe1aa0bc8f8eddbc24522f2fe92d37bc1
Rodolfo <rodolfo.cassaro@auaha.net>
2023-07-19 08:33:14 -0300

    component experiencie bar basico criado

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { ExperiencieBar } from "./components/ExperienceBar";
 import "./styles/global.css"
 
 function App() {
   return (
-    <>
-    
-    </>
+    <div className="container">
+    <ExperiencieBar />
+    </div>
   );
 }
 

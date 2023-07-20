5e8b7b82cd5e931b429819758217b752d6453f37
Rodolfo <rodolfo.cassaro@auaha.net>
2023-07-20 08:36:44 -0300

    lógica do countDown feita até o ponto do contador ser funcional

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+"use client"
 import { ExperiencieBar } from '@/src/components/ExperienceBar'
 import { CompletedChallenges } from '@/src/components/CompletedChallenges'
 import { Profile } from '@/src/components/Profile'
 
 import styles from "../src/styles/pages/Home.module.css"
-import { Countdown } from '@/src/components/Countdown'
+import {Countdown} from "../src/components/Countdown"
 
 
 
